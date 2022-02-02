c7732bc218d3db681477763f02d84c6f795703bf
rakhiagr <rakhiagr2020@gmail.com>
2022-02-02 14:27:16 -0700

    labels on chord

--- front-end/src/Components/Chord.jsx
+++ front-end/src/Components/Chord.jsx
@@ -30,9 +30,9 @@ const Chord = (props) => {
                     category_mapper[embeddings[i].id] = embeddings[i].category;
                 }
             }
-            console.log(category_mapper);
+            // console.log(category_mapper);
             let id = Object.keys(data);
-            console.log(data);
+            // console.log(data);
             for(const key in id){
                 const item = data[id[key]];
                 for(var i = 0; i<10; i++){
@@ -40,7 +40,7 @@ const Chord = (props) => {
                     data[id[key]][i] = 0;
                 }
             }
-            console.log(data);
+            // console.log(data);
             d3.select(svgRef.current).selectAll("*").remove();
 
             const index_task_id_map = {}
@@ -51,6 +51,7 @@ const Chord = (props) => {
                 count = count +1;
                 matrix.push(data[key]);
             }
+            // console.log(matrix);
 
             const outerRadius = (svgRef.current.clientWidth/2) - 170;
             const innerRadius = outerRadius - 15;
@@ -59,7 +60,7 @@ const Chord = (props) => {
             const opacityDefault = 0.8;
 
             const chord = d3.chord()
-                .padAngle(0.1);
+                .padAngle(0.09);
             const arc = d3.arc()
                 .innerRadius(innerRadius)
                 .outerRadius(outerRadius);
@@ -85,22 +86,43 @@ const Chord = (props) => {
                 .attr("id", function(d, i) { return "group" + d.index; })
                 .attr("d", arc);
 
-
+            // outerCircle.append("text")
+            //     .each(function(d) { d.angle = (d.startAngle + d.endAngle) / 2; })
+            //     .attr("dy", ".35em")
+            //     .attr("class", "titles")
+            //     .attr("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+            //     .attr("transform", function(d) {
+            //       return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+            //       + "translate(" + (outerRadius + 4) + ")"
+            //       + (d.angle > Math.PI ? "rotate(180)" : "");
+            //     })
+            //     .text(function(chords, i){
+            //         return category_mapper[Object.keys(props.taskNeighbours[i])[0]].split(" ").join("\n");
+            //     })
+            //     .style("font-size", 10);
 
             outerCircle.append("text")
-                .each(function(d) { d.angle = (d.startAngle + d.endAngle) / 2; })
-                .attr("dy", ".35em")
-                .attr("class", "titles")
-                .attr("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
-                .attr("transform", function(d) {
-                  return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
-                  + "translate(" + (outerRadius + 4) + ")"
-                  + (d.angle > Math.PI ? "rotate(180)" : "");
-                })
+            .attr("class", "titles")
+                .attr("x", 9)
+                .attr("dy", -20)
+                .append("textPath")
+                .attr("xlink:href", function(d) { return "#group" + d.index; })
                 .text(function(chords, i){
-                    return category_mapper[Object.keys(props.taskNeighbours[i])[0]].split(" ").join("\n");
+                    let l = category_mapper[Object.keys(props.taskNeighbours[i])[0]];
+                    // console.log(l);
+                    //l = l.split(" ").join("\n");
+                    // console.log(l);
+                    return l.split(" ")[0];
                 })
-                .style("font-size", 10);
+                .append("tspan")
+                    .attr("x", 9)
+                    .attr("dy", 13)
+                    .text(function(chords, i) {
+                        return category_mapper[Object.keys(props.taskNeighbours[i])[0]].split(" ")[1];
+                    })
+                .style("fill", "black")
+                .style("font-size", "10px")
+                .style("font-weight", "14");
 
             svg.selectAll("path.chord")
                 .data(function(chords) { return chords; })
